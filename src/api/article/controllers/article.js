3e23b168793276a5998418f2618c779929fa2248
Fedotos228 <tudor.onceanu@gmail.com>
2023-09-08 16:07:59 +0300

    First commit

---
+++ src/api/article/controllers/article.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * article controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::article.article', ({ strapi }) => ({
+    async findOne(ctx) {
+        const { id } = ctx.params;
+
+        const entity = await strapi.db.query("api::article.article").findOne({
+            where: { slug: id },
+            populate: ['deep']
+        })
+
+        const sanitizedResults = await this.sanitizeOutput(entity, ctx);
+
+        return this.transformResponse(sanitizedResults);
+    }
+}));
