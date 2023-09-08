c54167780f93d35c56f24da9f8fdda65e85656f2
Fedotos228 <tudor.onceanu@gmail.com>
2023-09-08 16:35:52 +0300

    vercel config

--- config/plugins.js
+++ config/plugins.js
@@ -2,4 +2,15 @@ module.exports = ({ env }) => ({
   seo: {
     enabled: true,
   },
+  "vercel-deploy": {
+    enabled: true,
+    config: {
+      deployHook:
+        "https://api.vercel.com/v1/integrations/deploy/prj_HgypgXDWG765xK9vOx3B2UJqcXq9/b6jCJvyLxp",
+      apiToken: "0s71feYVNGing7wzQf4IRMcB",
+      appFilter: "moldova-europeana-back",
+      teamFilter: "your-team-id-on-vercel",
+      roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+    },
+  },
 });
